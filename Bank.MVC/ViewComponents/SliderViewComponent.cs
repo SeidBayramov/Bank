d62b7bba2b8f02a7ffb176800fc577281b0a0c63
SeidBayramov <bd6gq7p0j@code.edu.az>
2024-02-04 00:53:13 +0400

    Slider wirte with component and some delete,recover

---
+++ Bank.MVC/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Bank.Business.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.MVC.ViewComponents
+{
+    public class SliderViewComponent:ViewComponent
+    {
+        private readonly ISliderService _service;
+
+        public SliderViewComponent(ISliderService service)
+        {
+            _service = service;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var services = (await _service.GetAllAsync()).Where(x => !x.IsDeleted).ToList();
+
+            return View(services);
+        }
+    }
+}
