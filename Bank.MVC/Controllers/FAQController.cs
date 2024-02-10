b52985c8301b8027be7ead4398eeabae9249ee51
SeidBayramov <bd6gq7p0j@code.edu.az>
2024-02-10 11:48:02 +0400

    Index was almost ready

---
+++ Bank.MVC/Controllers/FAQController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.MVC.Controllers
+{
+    public class FAQController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
